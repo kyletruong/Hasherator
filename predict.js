a4c1ad77f804b27bc0b9df4d96b32c7427f37d28
kyletruong <kyletruong@gmail.com>
2018-11-17 23:15:52 -0800

    fix layout

--- predict.js
+++ predict.js
@@ -60,138 +60,138 @@ function doPredict(value) {
       var tagArray, regionArray;
       var tagCount = 0;
       var modelName = response.rawData.outputs[0].model.name;
-      var modelNameShort = modelName.split("-")[0];
-      var modelHeader = '<b><span style="font-size:14px">' + capitalize(modelNameShort) + ' Model</span></b>';
+      //var modelNameShort = modelName.split("-")[0];
+      var modelHeader = '<b><span style="font-size:14px">' + 'Concepts</span></b>';
       
-      // Check for regions models first
-      if(response.rawData.outputs[0].data.hasOwnProperty("regions")) {
-        regionArray = response.rawData.outputs[0].data.regions;
+    //   // Check for regions models first
+    //   if(response.rawData.outputs[0].data.hasOwnProperty("regions")) {
+    //     regionArray = response.rawData.outputs[0].data.regions;
       	
-        // Regions are found, so iterate through all of them
-        for(var i = 0; i < regionArray.length; i++) {
-      	  conceptNames += "<b>Result " + (i+1) + "</b>";  
+    //     // Regions are found, so iterate through all of them
+    //     for(var i = 0; i < regionArray.length; i++) {
+    //   	  conceptNames += "<b>Result " + (i+1) + "</b>";  
       		 		
-      	  // Demographic has separate sub-arrays
-      	  if(modelName == "demographics") {
-      	    var ageArray = regionArray[i].data.face.age_appearance.concepts;
-      	    var ethnicArray = regionArray[i].data.face.multicultural_appearance.concepts;
-      	    var genderArray = regionArray[i].data.face.gender_appearance.concepts;
+    //   	  // Demographic has separate sub-arrays
+    //   	  if(modelName == "demographics") {
+    //   	    var ageArray = regionArray[i].data.face.age_appearance.concepts;
+    //   	    var ethnicArray = regionArray[i].data.face.multicultural_appearance.concepts;
+    //   	    var genderArray = regionArray[i].data.face.gender_appearance.concepts;
           
-            // Age Header
-      	    conceptNames += '<br/><b><span style="font-size:10px">Age Appearance</span></b>';
+    //         // Age Header
+    //   	    conceptNames += '<br/><b><span style="font-size:10px">Age Appearance</span></b>';
       		
-      	    // print top 5 ages
-      	    for(var a = 0; a < 5; a++) {
-      	      conceptNames += '<li>' + ageArray[a].name + ': <i>' + ageArray[a].value + '</i></li>'; 
-            }
+    //   	    // print top 5 ages
+    //   	    for(var a = 0; a < 5; a++) {
+    //   	      conceptNames += '<li>' + ageArray[a].name + ': <i>' + ageArray[a].value + '</i></li>'; 
+    //         }
       		
-      	    // Ethnicity Header
-      	    conceptNames += '<b><span style="font-size:10px">Multicultural Appearance</span></b>';
+    //   	    // Ethnicity Header
+    //   	    conceptNames += '<b><span style="font-size:10px">Multicultural Appearance</span></b>';
       			
-      	    // print top 3 ethnicities
-      	    for(var e = 0; e < 3; e++) {
-      	      conceptNames += '<li>' + ethnicArray[e].name + ': <i>' + ethnicArray[e].value + '</i></li>'; 
-            }
+    //   	    // print top 3 ethnicities
+    //   	    for(var e = 0; e < 3; e++) {
+    //   	      conceptNames += '<li>' + ethnicArray[e].name + ': <i>' + ethnicArray[e].value + '</i></li>'; 
+    //         }
             
-      	    // Gender Header
-      	    conceptNames += '<b><span style="font-size:10px">Gender Appearance</span></b>';
+    //   	    // Gender Header
+    //   	    conceptNames += '<b><span style="font-size:10px">Gender Appearance</span></b>';
       		
-      	    // print gender
-      	    conceptNames += '<li>' + genderArray[0].name + ': <i>' + genderArray[0].value + '</i></li>'; 
-      	}
+    //   	    // print gender
+    //   	    conceptNames += '<li>' + genderArray[0].name + ': <i>' + genderArray[0].value + '</i></li>'; 
+    //   	}
       		
-        // For faces just print bounding boxes
-      	else if(modelName == "face-v1.3") {
-      	  // Top Row
-      	  conceptNames += '<li>Top Row: <i>' + regionArray[i].region_info.bounding_box.top_row + '</i></li>';
-      	  conceptNames += '<li>Left Column: <i>' + regionArray[i].region_info.bounding_box.left_col + '</i></li>';
-      	  conceptNames += '<li>Bottom Row: <i>' + regionArray[i].region_info.bounding_box.bottom_row + '</i></li>';
-      	  conceptNames += '<li>Right Column: <i>' + regionArray[i].region_info.bounding_box.right_col + '</i></li>';
-      	}
+    //     // For faces just print bounding boxes
+    //   	else if(modelName == "face-v1.3") {
+    //   	  // Top Row
+    //   	  conceptNames += '<li>Top Row: <i>' + regionArray[i].region_info.bounding_box.top_row + '</i></li>';
+    //   	  conceptNames += '<li>Left Column: <i>' + regionArray[i].region_info.bounding_box.left_col + '</i></li>';
+    //   	  conceptNames += '<li>Bottom Row: <i>' + regionArray[i].region_info.bounding_box.bottom_row + '</i></li>';
+    //   	  conceptNames += '<li>Right Column: <i>' + regionArray[i].region_info.bounding_box.right_col + '</i></li>';
+    //   	}
       		
-      	// Celebrity
-      	else if(modelName == "celeb-v1.3") {
-      	  tagArray = regionArray[i].data.face.identity.concepts;
+    //   	// Celebrity
+    //   	else if(modelName == "celeb-v1.3") {
+    //   	  tagArray = regionArray[i].data.face.identity.concepts;
       			
-      	  // Print first 10 results
-      	  for(var c=0; c < 10; c++) {
-      	    conceptNames += '<li>' + tagArray[c].name + ': <i>' + tagArray[c].value + '</i></li>'; 
-          }
-      	}
+    //   	  // Print first 10 results
+    //   	  for(var c=0; c < 10; c++) {
+    //   	    conceptNames += '<li>' + tagArray[c].name + ': <i>' + tagArray[c].value + '</i></li>'; 
+    //       }
+    //   	}
       		
-      	// Logos
-      	else if(modelName == "logo") {
-      	  // Print all results
-      	  conceptNames += '<br/><b><span style="font-size:10px">Logo</span></b>';
-      	  conceptNames += '<li>' + regionArray[i].data.concepts[0].name + ': <i>' + regionArray[i].data.concepts[0].value + '</i></li>';
-      	  conceptNames += '<br/><b><span style="font-size:10px">Location</span></b>';
-      	  conceptNames += '<li>Top Row: <i>' + regionArray[i].region_info.bounding_box.top_row + '</i></li>';
-      	  conceptNames += '<li>Left Column: <i>' + regionArray[i].region_info.bounding_box.left_col + '</i></li>';
-      	  conceptNames += '<li>Bottom Row: <i>' + regionArray[i].region_info.bounding_box.bottom_row + '</i></li>';
-      	  conceptNames += '<li>Right Column: <i>' + regionArray[i].region_info.bounding_box.right_col + '</i></li>';
-      	}
+    //   	// Logos
+    //   	else if(modelName == "logo") {
+    //   	  // Print all results
+    //   	  conceptNames += '<br/><b><span style="font-size:10px">Logo</span></b>';
+    //   	  conceptNames += '<li>' + regionArray[i].data.concepts[0].name + ': <i>' + regionArray[i].data.concepts[0].value + '</i></li>';
+    //   	  conceptNames += '<br/><b><span style="font-size:10px">Location</span></b>';
+    //   	  conceptNames += '<li>Top Row: <i>' + regionArray[i].region_info.bounding_box.top_row + '</i></li>';
+    //   	  conceptNames += '<li>Left Column: <i>' + regionArray[i].region_info.bounding_box.left_col + '</i></li>';
+    //   	  conceptNames += '<li>Bottom Row: <i>' + regionArray[i].region_info.bounding_box.bottom_row + '</i></li>';
+    //   	  conceptNames += '<li>Right Column: <i>' + regionArray[i].region_info.bounding_box.right_col + '</i></li>';
+    //   	}
       		
-      	// Focus
-      	else if(modelName == "focus") {
-      	  // Print total focus score and all regions with focus
-      	  conceptNames += '<br/><b><span style="font-size:10px">Focus Region</span></b>';
-      	  conceptNames += '<li>Top Row: <i>' + regionArray[i].region_info.bounding_box.top_row + '</i></li>';
-      	  conceptNames += '<li>Left Column: <i>' + regionArray[i].region_info.bounding_box.left_col + '</i></li>';
-      	  conceptNames += '<li>Bottom Row: <i>' + regionArray[i].region_info.bounding_box.bottom_row + '</i></li>';
-      	  conceptNames += '<li>Right Column: <i>' + regionArray[i].region_info.bounding_box.right_col + '</i></li>';
+    //   	// Focus
+    //   	else if(modelName == "focus") {
+    //   	  // Print total focus score and all regions with focus
+    //   	  conceptNames += '<br/><b><span style="font-size:10px">Focus Region</span></b>';
+    //   	  conceptNames += '<li>Top Row: <i>' + regionArray[i].region_info.bounding_box.top_row + '</i></li>';
+    //   	  conceptNames += '<li>Left Column: <i>' + regionArray[i].region_info.bounding_box.left_col + '</i></li>';
+    //   	  conceptNames += '<li>Bottom Row: <i>' + regionArray[i].region_info.bounding_box.bottom_row + '</i></li>';
+    //   	  conceptNames += '<li>Right Column: <i>' + regionArray[i].region_info.bounding_box.right_col + '</i></li>';
           
-          if(i === regionArray.length - 1) {
-      		  conceptNames += '<br><br><li><br><br>Overall Focus: <i>' + response.rawData.outputs[0].data.focus.value + '</i><br><br><br></li>'; 
-      		}
-      	}
+    //       if(i === regionArray.length - 1) {
+    //   		  conceptNames += '<br><br><li><br><br>Overall Focus: <i>' + response.rawData.outputs[0].data.focus.value + '</i><br><br><br></li>'; 
+    //   		}
+    //   	}
 
-      	tagCount+=10;      	
-      }
-     }
+    //   	tagCount+=10;      	
+    //   }
+    //  }
       
-      // Color Model
-      else if(modelName === "color") {
-      	conceptNames += '<b><span style="font-size:10px">Colors</span></b>';
-        tagArray = response.rawData.outputs[0].data.colors;
+    //   // Color Model
+    //   else if(modelName === "color") {
+    //   	conceptNames += '<b><span style="font-size:10px">Colors</span></b>';
+    //     tagArray = response.rawData.outputs[0].data.colors;
         
-        for (var col = 0; col < tagArray.length; col++) {
-          conceptNames += '<li>' + tagArray[col].w3c.name + ': <i>' + tagArray[col].value + '</i></li>';
-        }
+    //     for (var col = 0; col < tagArray.length; col++) {
+    //       conceptNames += '<li>' + tagArray[col].w3c.name + ': <i>' + tagArray[col].value + '</i></li>';
+    //     }
         
-        tagCount=tagArray.length;
-      }
+    //     tagCount=tagArray.length;
+    //   }
       
       // Generic tag response models
-      else if(response.rawData.outputs[0].data.hasOwnProperty("concepts")) {
+      if (response.rawData.outputs[0].data.hasOwnProperty("concepts")) {
         tagArray = response.rawData.outputs[0].data.concepts;
         
         for (var other = 0; other < tagArray.length; other++) {
-          conceptNames += '<li>' + tagArray[other].name + ': <i>' + tagArray[other].value + '</i></li>';
+          conceptNames += '<li>' + tagArray[other].name; //+ tagArray[other].value + '</i></li>';
         }
         
         tagCount=tagArray.length;
       }
       
       // Bad region request
-      else {
-      	if(modelName != "logo" && modelName != "focus") {
-          $('#concepts').html("<br/><br/><b>No Faces Detected!</b>");
-        }
-      	else if(modelName == "logo") {
-          $('#concepts').html("<br/><br/><b>No Logos Detected!</b>");
-        }
-        else {
-          $('#concepts').html("<br/><br/><b>No Focus Regions Detected!</b>");
-        }
-      	return;
-      }
+      // else {
+      // 	if(modelName != "logo" && modelName != "focus") {
+      //     $('#concepts').html("<br/><br/><b>No Faces Detected!</b>");
+      //   }
+      // 	else if(modelName == "logo") {
+      //     $('#concepts').html("<br/><br/><b>No Logos Detected!</b>");
+      //   }
+      //   else {
+      //     $('#concepts').html("<br/><br/><b>No Focus Regions Detected!</b>");
+      //   }
+      // 	return;
+      // }
       
       var columnCount = tagCount / 10;
       
-      // Focus gets one more column
-      if(modelName == "focus") {
-      	columnCount += 1;
-      }
+      // // Focus gets one more column
+      // if(modelName == "focus") {
+      // 	columnCount += 1;
+      // }
       
       conceptNames = '<ul style="margin-right:20px; margin-top:20px; columns:' + columnCount + '; -webkit-columns:' + columnCount + '; -moz-columns:' + columnCount + ';">' + conceptNames;
         
@@ -214,76 +214,77 @@ function doPredict(value) {
     Back-end model id
 */
 function getSelectedModel() {
-  var model = document.querySelector('input[name = "model"]:checked').value;
+  return Clarifai.GENERAL_MODEL;
+  // var model = document.querySelector('input[name = "model"]:checked').value;
   
-  if(model === "general") {
-    return Clarifai.GENERAL_MODEL;
-  }
+  // if(model === "general") {
+  //   return Clarifai.GENERAL_MODEL;
+  // }
     
-  else if(model === "food") {
-    return Clarifai.FOOD_MODEL;
-  }
+  // else if(model === "food") {
+  //   return Clarifai.FOOD_MODEL;
+  // }
     
-  else if(model === "NSFW") {
-    return Clarifai.NSFW_MODEL;
-  }
+  // else if(model === "NSFW") {
+  //   return Clarifai.NSFW_MODEL;
+  // }
     
-  else if(model === "travel") {
-    return Clarifai.TRAVEL_MODEL;
-  }
+  // else if(model === "travel") {
+  //   return Clarifai.TRAVEL_MODEL;
+  // }
     
-  else if(model === "wedding") {
-    return Clarifai.WEDDINGS_MODEL;
-  }
+  // else if(model === "wedding") {
+  //   return Clarifai.WEDDINGS_MODEL;
+  // }
     
-  else if(model === "color") {
-    return Clarifai.COLOR_MODEL;
-  }
+  // else if(model === "color") {
+  //   return Clarifai.COLOR_MODEL;
+  // }
   
-  else if(model === "demographic") {
-    return Clarifai.DEMOGRAPHICS_MODEL;
-  }
+  // else if(model === "demographic") {
+  //   return Clarifai.DEMOGRAPHICS_MODEL;
+  // }
   
-  else if(model === "logo") {
-    return Clarifai.LOGO_MODEL;
-  }
+  // else if(model === "logo") {
+  //   return Clarifai.LOGO_MODEL;
+  // }
   
-  else if(model === "apparel") {
-    return "e0be3b9d6a454f0493ac3a30784001ff";
-  }
+  // else if(model === "apparel") {
+  //   return "e0be3b9d6a454f0493ac3a30784001ff";
+  // }
   
-  else if(model === "faces") {
-    return Clarifai.FACE_DETECT_MODEL;
-  }
+  // else if(model === "faces") {
+  //   return Clarifai.FACE_DETECT_MODEL;
+  // }
   
-  else if(model == "focus") {
-    return Clarifai.FOCUS_MODEL;
-  }
+  // else if(model == "focus") {
+  //   return Clarifai.FOCUS_MODEL;
+  // }
   
-  else if(model === "celebrity") {
-    return "e466caa0619f444ab97497640cefc4dc";
-  }
+  // else if(model === "celebrity") {
+  //   return "e466caa0619f444ab97497640cefc4dc";
+  // }
   
-  else if(model === "moderation") {
-    return "d16f390eb32cad478c7ae150069bd2c6";
-  }
+  // else if(model === "moderation") {
+  //   return "d16f390eb32cad478c7ae150069bd2c6";
+  // }
   
-  else if(model === "portrait") {
-    return "de9bd05cfdbf4534af151beb2a5d0953";
-  }
+  // else if(model === "portrait") {
+  //   return "de9bd05cfdbf4534af151beb2a5d0953";
+  // }
   
-  else if(model === "landscape") {
-    return "bec14810deb94c40a05f1f0eb3c91403";
-  }
+  // else if(model === "landscape") {
+  //   return "bec14810deb94c40a05f1f0eb3c91403";
+  // }
   
-  else if(model == "texturespatterns") {
-    return "fbefb47f9fdb410e8ce14f24f54b47ff";
-  }
+  // else if(model == "texturespatterns") {
+  //   return "fbefb47f9fdb410e8ce14f24f54b47ff";
+  // }
   
-  else if(model === "custom") {
-    var e = document.getElementById("custom_models_dropdown");
-    return e.options[e.selectedIndex].value;
-  }
+  // else if(model === "custom") {
+  //   var e = document.getElementById("custom_models_dropdown");
+  //   return e.options[e.selectedIndex].value;
+  // }
 }
 
 /*
@@ -291,36 +292,36 @@ function getSelectedModel() {
   Args:
     index - # of the image in the session
 */
-function addImageToApp(index) {
-  var imgType = document.getElementById("hidden-type" + index).value;
-  var imgValue = document.getElementById("hidden-val" + index).value;
+// function addImageToApp(index) {
+//   var imgType = document.getElementById("hidden-type" + index).value;
+//   var imgValue = document.getElementById("hidden-val" + index).value;
   
-  if(imgType === "url") {
-    app.inputs.create({
-      url: imgValue
-    }).then(
-      function(response) {
-        alert("Image successfully added!");
-      },
-      function(err) {
-        alert("Error Adding Image. Check to see if it is a duplicate.");
-      }
-    );
-  }
+//   if(imgType === "url") {
+//     app.inputs.create({
+//       url: imgValue
+//     }).then(
+//       function(response) {
+//         alert("Image successfully added!");
+//       },
+//       function(err) {
+//         alert("Error Adding Image. Check to see if it is a duplicate.");
+//       }
+//     );
+//   }
   
-  else if(imgType === "base64") {
-    app.inputs.create({
-      base64: imgValue
-    }).then(
-      function(response) {
-        alert("Image successfully added!");
-      },
-      function(err) {
-        alert("Error Adding Image. Check to see if it is a duplicate.");
-      }
-    );
-  }
-}
+//   else if(imgType === "base64") {
+//     app.inputs.create({
+//       base64: imgValue
+//     }).then(
+//       function(response) {
+//         alert("Image successfully added!");
+//       },
+//       function(err) {
+//         alert("Error Adding Image. Check to see if it is a duplicate.");
+//       }
+//     );
+//   }
+// }
 
 /*
   Purpose: Create a dynamic div to store entire user session
@@ -335,9 +336,7 @@ function createNewDisplayDiv(index) {
     '<div style="margin-top:30px; margin-left:20px; margin-bottom:30px; clear:left; float:left"> \
       <img id="img_preview' + index + '" src="" width="400"/> \
       <br/> \
-      <span id="add-image-button" style="visibility:hidden"> \
-        <button onClick="addImageToApp(' + index + ')">Add image to application</button> \
-      </span> \
+            \
     </div> \
     <div id="concepts" class="conceptBox"> \
     </div>';
